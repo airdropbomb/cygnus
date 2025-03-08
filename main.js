fc669a5916724ef778b7ea63e64df3834bb3234d
airdropbomb <saginaw135@gmail.com>
2025-03-08 17:28:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const headers = {
     'accept-encoding': 'gzip, deflate, br, zstd',
     'accept-language': 'en-US,en;q=0.8',
     'content-type': 'application/json',
-    'Authorization': 'Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1HT1FOYWNITnJBZ2RSdVVUOVl2NTNtU0k3Y0gxVHVBOXJ0dERZbmlkS1EifQ.eyJzaWQiOiJjbTgwMHY2dXIwMTloY3ljeWFvd2VyZjlpIiwiaXNzIjoicHJpdnkuaW8iLCJpYXQiOjE3NDE0MjczMjQsImF1ZCI6ImNtN2Y3Y2E4bTAwM3cxMXZmdDE1Mnd6N2YiLCJzdWIiOiJkaWQ6cHJpdnk6Y204MDB2NndvMDE5amN5Y3lhNHJybWlzZCIsImV4cCI6MTc0MTQzMDkyNH0.8MEM6VofgvX-RlEa7Zb75M2GcSuYs6XFed4g1U4fSTa7PWlFci8apdrp4zmZAoCNOFszPqI2Eiuijxjw7PMnCA', // Token ကို ဒီနေရာမှာ ထည့်ပါ
+    'cookie': 'ISI COOKIE KAMU', //GANTI INI
     'origin': 'https://i.cygnus.finance',
     'referer': 'https://i.cygnus.finance/',
     'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Brave";v="134"',
@@ -21,8 +21,8 @@ const headers = {
 };
 
 const data = {
-    id: "did:privy:cm800v6wo019jcycya4rrmisd",
-    power: 100,
+    id: "ISI ID KAMU", //GANTI INI
+    power: ISI POWER SESUKA HATI , //GANTI INI
     energy: 10,
     lastPlayAt: "2025-03-08T09:51:57.566Z"
 };
